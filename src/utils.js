d9ff230a490600acdac959bafc625a5c52acbc1d
Safin Ali <safin.ali.7205@gmail.com>
2024-02-05 02:32:53 +0600

    feat: :wheelchair: random user agent more unique

--- src/utils.js
+++ src/utils.js
@@ -3,20 +3,22 @@ import { config } from "dotenv";
 config();
 
 export const getRandomUserAgent = () => {
-	const userAgents = [
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15",
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15",
-		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/97.0.4692.71 Chrome/97.0.4692.71 Safari/537.36",
-		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/97.0.4692.71 Safari/537.36",
-		// Add more user agents as needed
-	];
-
-	const randomIndex = Math.floor(Math.random() * userAgents.length);
-	return userAgents[randomIndex];
-}
+	// Array of common components for generating random user agents
+	const components = {
+		os: ['Windows NT 10.0', 'Macintosh; Intel Mac OS X 10_15_7', 'Linux x86_64'],
+		browsers: [
+			{ name: 'Chrome', version: '97.0.' },
+			{ name: 'Firefox', version: '96.0.' },
+			{ name: 'Safari', version: '15.2' },
+			{ name: 'Edge', version: '97.0.' },
+			{ name: 'Opera', version: '83.0.' }
+		]
+	};
+	const os = components.os[Math.floor(Math.random() * components.os.length)];
+	const browser = components.browsers[Math.floor(Math.random() * components.browsers.length)];
+	const newAgent = `Mozilla/5.0 (${os}) AppleWebKit/537.36 (KHTML, like Gecko) ${browser.name}/${browser.version}${Math.floor(Math.random() * (100 - 999 + 1)) + 999}.99 Safari/537.36`;
+	userAgent = newAgent;
+};
 
 export const decryptString = (encryptedStr) => {
 	const key = Buffer.from(process.env.ENCRIPTION_KEY, 'hex');
